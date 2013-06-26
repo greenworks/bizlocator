72f2647f560cd80b3a3eecd72f946185d575ca41
Swapnil Joshi <joshi4u@gmail.com>
2013-06-27 01:52:03 +0530

    ad_type added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515183040) do
+ActiveRecord::Schema.define(:version => 20130626193528) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(:version => 20130515183040) do
     t.boolean  "ispublished"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "ad_types", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "addresses", :force => true do |t|
@@ -99,6 +105,7 @@ ActiveRecord::Schema.define(:version => 20130515183040) do
     t.datetime "updated_at",       :null => false
     t.integer  "user_id"
     t.string   "image"
+    t.integer  "ad_type_id"
   end
 
   create_table "bookmark_ads", :force => true do |t|
