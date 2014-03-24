0f6878594a710bf530718f598908af892efd4865
Swapnil Joshi <swapnil.joshi@haatopia.com>
2014-03-24 22:19:36 +0530

    relationships

--- app/models/ad_categories_notification.rb
+++ app/models/ad_categories_notification.rb
@@ -1,6 +1,7 @@
 class AdCategoriesNotification < ActiveRecord::Base
   attr_accessible :ad_category_id, :name, :user_id
 
-  has_many :users
-  has_many :ad_categories
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :ad_categories
+
 end
