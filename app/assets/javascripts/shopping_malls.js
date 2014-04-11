65bb137fc9dcecbb4903bc85b5a223b5992c4c0b
Swapnil Joshi <swapnil.joshi@haatopia.com>
2014-04-11 22:16:07 +0530

    hidden blockes removed

--- app/assets/javascripts/shopping_malls.js
+++ app/assets/javascripts/shopping_malls.js
@@ -3,55 +3,55 @@
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 // getPosition
-function xy(x) {
-o = document.getElementById(x);
-var l =o.offsetLeft; var t = o.offsetTop;
-while (o=o.offsetParent)
-  l += o.offsetLeft;
-o = document.getElementById(x);
-while (o=o.offsetParent)
-  t += o.offsetTop;
-return [l,t];
-}
-
-// SHOW POP-OVER
-function showPopOver(divID,parentID) {
-// SET THE DIV POSITION
-// SHOW THE DIV ()
-
-document.getElementById(divID).style.display = "inline-flex";
-
-
-//var parent_x= parseInt(xy(parentID)[1]);
-        var parent_y = parseInt(xy(parentID)[0]);
-        var div_height=	parseInt(document.getElementById(divID).offsetHeight);
-        var pop_pos = parent_y - div_height - 10;
-
-    }
-
-    // CLOSE POP-OVER
-function closePopOver(divID) {
-// HIDE THE DIV
-document.getElementById(divID).style.display = "none";
-}
-
-// SHOW POP-OVER
-function showPopInfo(divID,parentID) {
-// SET THE DIV POSITION
-// SHOW THE DIV ()
-
-document.getElementById(divID).style.display = "inline-flex";
-
-
-var parent_y = parseInt(xy(parentID)[1]);
-var parent_y = parseInt(xy(parentID)[0]);
-var div_height=	parseInt(document.getElementById(divID).offsetHeight);
-var pop_pos = parent_y - div_height - 10;
-
-}
-
-// CLOSE POP-OVER
-function closePopInfo(divID) {
-// HIDE THE DIV
-document.getElementById(divID).style.display = "none";
-}
+//function xy(x) {
+//o = document.getElementById(x);
+//var l =o.offsetLeft; var t = o.offsetTop;
+//while (o=o.offsetParent)
+//  l += o.offsetLeft;
+//o = document.getElementById(x);
+//while (o=o.offsetParent)
+//  t += o.offsetTop;
+//return [l,t];
+//}
+//
+//// SHOW POP-OVER
+//function showPopOver(divID,parentID) {
+//// SET THE DIV POSITION
+//// SHOW THE DIV ()
+//
+//document.getElementById(divID).style.display = "inline-flex";
+//
+//
+////var parent_x= parseInt(xy(parentID)[1]);
+//        var parent_y = parseInt(xy(parentID)[0]);
+//        var div_height=	parseInt(document.getElementById(divID).offsetHeight);
+//        var pop_pos = parent_y - div_height - 10;
+//
+//    }
+//
+//    // CLOSE POP-OVER
+//function closePopOver(divID) {
+//// HIDE THE DIV
+//document.getElementById(divID).style.display = "none";
+//}
+//
+//// SHOW POP-OVER
+//function showPopInfo(divID,parentID) {
+//// SET THE DIV POSITION
+//// SHOW THE DIV ()
+//
+//document.getElementById(divID).style.display = "inline-flex";
+//
+//
+//var parent_y = parseInt(xy(parentID)[1]);
+//var parent_y = parseInt(xy(parentID)[0]);
+//var div_height=	parseInt(document.getElementById(divID).offsetHeight);
+//var pop_pos = parent_y - div_height - 10;
+//
+//}
+//
+//// CLOSE POP-OVER
+//function closePopInfo(divID) {
+//// HIDE THE DIV
+//document.getElementById(divID).style.display = "none";
+//}
